840acadd2c63edbfc7ee77ae8b2f61ddbd1a3675
mastii <srivastavaharshit1007@gmail.com>
2025-11-10 17:28:54 +0530

    feat(server): bootstrap TypeScript Express app with /health route

---
+++ server/src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(helmet());
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get("/health", (_req, res) => {
+  res.json({ ok: true });
+});
+
+export default app;
